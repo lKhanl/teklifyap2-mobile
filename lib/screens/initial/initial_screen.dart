e6aa573f7a3da505a3dbd23a4345428b13170fce
Khan <oguzhanercelik@eskisehir.edu.tr>
2023-01-02 22:07:41 +0300

    init page updated

--- lib/screens/initial/initial_screen.dart
+++ lib/screens/initial/initial_screen.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:teklifyap_mobil2/screens/login/login_screen.dart';
 import 'package:teklifyap_mobil2/screens/register/register_screen.dart';
-
+import 'package:teklifyap_mobil2/style/colors.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({Key? key}) : super(key: key);
@@ -16,47 +16,48 @@ class InitialScreen extends StatefulWidget {
 class _InitialScreenState extends State<InitialScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children:  [
-        const Text(
-          "teklifyap",
-          style: TextStyle(color: Colors.black
+    return Container(
+      color: ThemeColors.primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "teklifyap",
+            style: TextStyle(
+                color: Colors.black,
+                decoration: TextDecoration.none,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Roboto",
+                fontStyle: FontStyle.italic),
           ),
-        ),
-
-        const Padding(
-            padding: EdgeInsets.all(40)
-        ),
-
-        ElevatedButton(
-          onPressed: () {
-            Get.offAll(() => const LoginScreen());
-          },
-          style: ElevatedButton.styleFrom(
-              fixedSize: const Size(130, 40),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50))),
-          child: const Text('Sign In',style: TextStyle(color: Colors.black)),
-
-        ),
-
-
-        const Padding(
-            padding: EdgeInsets.all(10)
-        ),
-
-        ElevatedButton(
-          onPressed: () {
-            Get.offAll(() => const RegisterScreen());
-          },
-          style: ElevatedButton.styleFrom(
-              fixedSize: const Size(130, 40),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50))),
-          child: const Text('Register',style: TextStyle(color: Colors.black)),
-
-        )       ],
+          const SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {
+              Get.offAll(() => const LoginScreen());
+            },
+            style: ElevatedButton.styleFrom(
+                fixedSize: const Size(130, 40),
+                primary: ThemeColors.secondaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50))),
+            child: const Text('Sign In', style: TextStyle(color: Colors.black)),
+          ),
+          const Padding(padding: EdgeInsets.all(10)),
+          ElevatedButton(
+            onPressed: () {
+              Get.offAll(() => const RegisterScreen());
+            },
+            style: ElevatedButton.styleFrom(
+                fixedSize: const Size(130, 40),
+                primary: ThemeColors.secondaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50))),
+            child:
+                const Text('Register', style: TextStyle(color: Colors.black)),
+          )
+        ],
+      ),
     );
   }
 }
